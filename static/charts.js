7a74c377ec311d8222ed98c1476a8ca197dfead6
JackHast <jackhastings043@gmail.com>
2023-09-15 00:34:56 +1000

    update

--- static/charts.js
+++ static/charts.js
@@ -1,52 +1,109 @@
-const url = ''
+const url = "http://127.0.0.1:5000/api/v1.0/counts"
 
-function BarChart(){
+function BarChart(input) {
     d3.json(url).then(function(data) {
-       
-        
-        let bar_data = [{
-            x: ,
-            y: ,
-            text:,
-            type: "bar"
-        }];
-        Plotly.newPlot("bar", bar_data);
-})};
+        let year = input[0];
+        let option = input[1];
+        let Data = data[0]
+        let feature_data=''
+        if (option=='DEGS'){
+            feature_data=Data.DEGS 
+        }
+        else if (option=='LGA'){
+            feature_data=Data.LGA 
+        }
+        else if (option=='Region'){
+            feature_data=Data.region
+        }
+        else if (option=='Atmospheric Condition'){
+            feature_data=Data.atmos_cond
+        }
+        else if (option=='Surface Condition'){
+            feature_data=Data.surface_cond
+        }
+        else if (option=='Light Condition'){
+            feature_data=Data.light_cond
+        }
+        else if (option=='Speed Zone'){
+            feature_data=Data.speed_zone
+        }
+        else if (option=='Age Group'){
+            feature_data=Data.age_group
+        }
+        else if (option=='Vehicle Type'){
+            feature_data=Data.vehicle_type
+        }
+        else if (option=='Vehicle Brand'){
+            feature_data=Data.vehicle_brand
+        }
+        let feature_data_year_x = [];
+        let feature_data_year_y = [];
+        for (const [key, value] of Object.entries(feature_data)){
+            if (key.slice(-4)==year){
+                feature_data_year_x.push(key);
+                feature_data_year_y.push(value);
+            }
+        }
+        for (let i=0;i<feature_data_year_x.length;i++){
+            feature_data_year_x[i]=feature_data_year_x[i].toLowerCase()
+            feature_data_year_x[i]=feature_data_year_x[i].substring(0, feature_data_year_x[i].length-5)
+            if (option=='DEGS'){
+                feature_data_year_x[i] = feature_data_year_x[i].replace("_", " ")
+                if (feature_data_year_x[i]=="large provincial_cities"){
+                    feature_data_year_x[i]="large provincial cities"
+                }
+            }
+        }
+        // if (option=='LGA'){
+        //     for (let i=0;i<feature_data_year_y;i++){
+        //         if (){
 
-function Pie(){
-    d3.json(url).then(function(data) {
-        
-        let pie_data = [{
-            x: ,
-            y: ,
-            
-        }];
-        // Adding plot to div with id = "bubble" 
-        Plotly.newPlot("pie", pie_data);
-})};
+        //         }
+        //     }
+        // }
+            let bar_data = [{
+                x: feature_data_year_y,
+                y: feature_data_year_x,
+                type: "bar",
+                orientation:'h'
+            }];
+            let layout = {
+                yaxis: {
+                    automargin: true
+                  }
+              }
+            Plotly.newPlot("bar", bar_data,layout);
+        }
+    )};
 
 
-// Assigning variable to div with class = selDataset
-let dropdownMenu = d3.select("#selDataset");
+// // Assigning variable to div with class = selDatasetFeat
+let dropdownMenuFeat = d3.select("#selDatasetFeat");
+
+let dropdownMenuYear = d3.select("#selDatasetYear");
 
 function optionChanged(){
     // Finding option chosen
-    let ID = dropdownMenu.property("value");
+    let feature = dropdownMenuFeat.property("value");
+    let year = dropdownMenuYear.property("value");
     // Calling functions for option 
-    BarChart(ID);
-    Bubble(ID);
-    DemographicInfo(ID)
+    BarChart([year,feature]);
 }
 
 
 // This function plots default charts for when the page is vistited 
 function init(){
     // Plotting default charts 
-    BarChart();
-    Pie()
+    BarChart(['2020','DEGS']);
     // Put options in the dropdown menu 
-    d3.json(url).then(function(data) {
-    
-    })}
-    
+    years = [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+    features=['DEGS','LGA','Region','Atmospheric Condition','Surface Condition','Light Condition','Speed Zone','Age Group','Vehicle Type','Vehicle Brand']
+    for (let i = 0;i<features.length;i++){
+        dropdownMenuFeat.append("option").text(features[i]);
+    }
+    for (let i = 0;i<years.length;i++){
+        dropdownMenuYear.append("option").text(years[i]);
+    }
+};
+
 init()
--- static/cluster.js
+++ static/cluster.js
@@ -1,5 +1,5 @@
 
-url = "http://127.0.0.1:5000/api/v1.0/accident"
+const url = "http://127.0.0.1:5000/api/v1.0/accident"
 
 let myMap = L.map("map", {
   center: [-37.8136, 144.9631],
@@ -10,18 +10,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// var crashIcon = L.icon({
+//   iconUrl: 'leaf-green.png',
+//   iconSize:     [38, 95], // size of the icon
+//   iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
 
 d3.json(url).then(function(data) {
   let markers = L.markerClusterGroup();
   for (let i = 0; i < data.length; i++) {
     let dict =data[i]
     let coordinates = dict.coordinates
+    let speed = dict.speed_zone
+    if (dict.speed_zone==777){
+      speed = 'Other speed limit';
+    }
+    if (dict.speed_zone==888){
+      speed = 'Camping grounds, off road';
+    }
+    if (dict.speed_zone==999){
+      speed = 'Not known';
+    }
    
     markers.addLayer(L.marker([coordinates[0],coordinates[1]])
         .bindPopup("<h3>Date: " + dict.date + "<h3><h3>Time: " + dict.time + "<h3><h3>Surface Condition: " + dict.surface_cond + 
         "<h3><h3>Type: " + dict.accident_type + "<h3><h3>Fatalities: " + dict.no_persons_killed 
         + "<h3><h3>Light Condition: " + dict.light_cond + "<h3><h3>Atmospheric Condition: " + dict.atmosph_cond
-        + "<h3><h3>Speed Zone: " + dict.speed_zone + "<h3></h3>"));
+        + "<h3><h3>Speed Zone: " + speed + "<h3></h3>"));
 
   }
   myMap.addLayer(markers);
