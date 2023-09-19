14f3cf9748f57afcbab36c70957c3fef8d541660
KTamas03 <132874272+KTamas03@users.noreply.github.com>
2023-09-19 22:07:07 +1000

    update files

--- static/chartsfilter.js
+++ static/chartsfilter.js
@@ -13,31 +13,31 @@ function BarChart(input) {
         if (option=='Locality Type'){
             feature_data=Data.DEGS 
         }
-        else if (option=='LGA'){
+        else if (option=='LGA (Top 10)'){
             feature_data=Data.LGA 
         }
         else if (option=='Region'){
             feature_data=Data.region
         }
-        else if (option=='Atmospheric Condition'){
+        else if (option=='Atmospheric Condition (Top 10)'){
             feature_data=Data.atmos_cond
         }
-        else if (option=='Surface Condition'){
+        else if (option=='Surface Condition (Top 10)'){
             feature_data=Data.surface_cond
         }
         else if (option=='Light Condition'){
             feature_data=Data.light_cond
         }
-        else if (option=='Speed Zone'){
+        else if (option=='Speed Zone (Top 10)'){
             feature_data=Data.speed_zone
         }
         else if (option=='Age Group'){
             feature_data=Data.age_group
         }
-        else if (option=='Vehicle Type'){
+        else if (option=='Vehicle Type (Top 10)'){
             feature_data=Data.vehicle_type
         }
-        else if (option=='Vehicle Brand'){
+        else if (option=='Vehicle Brand (Top 10)'){
             feature_data=Data.vehicle_brand
         }
         let feat = [];
@@ -67,17 +67,23 @@ function BarChart(input) {
                     feat[i]="Large provincial cities "
                 }
             }
-            else if (option=='Vehicle Brand'){
+            else if (option=='Vehicle Brand (Top 10)'){ // Making corrections to vehicle brand names
                 feat[i]=`${feat[i]} `
                 feat[i]=feat[i][0].toUpperCase() + feat[i].slice(1)
                 if (feat[i] == '       '){
                     accident_count[i]=0;
                     }
-                else if (feat[i]=='Unkn  '){
+                else if (feat[i]=='Unkn   '){
                     feat[i]='Unknown '
                 }
+                else if (feat[i]=='Hyndai '){
+                    feat[i]='Hyundai '
+                }
+                else if (feat[i]=='Mitsub '){
+                    feat[i]='Mitsubishi '
+                }
             }
-            else if (option == 'Speed Zone'){
+            else if (option == 'Speed Zone (Top 10)'){ // Changing speed zone codes to names
                 if (feat[i]=='999'){
                     feat[i]='Unknown'
                 }
@@ -92,7 +98,7 @@ function BarChart(input) {
                 }
 
             }
-            else if (option == 'Vehicle Type'){
+            else if (option == 'Vehicle Type (Top 10)'){ 
                 feat[i]=`${feat[i]} `
                 feat[i]=feat[i][0].toUpperCase() + feat[i].slice(1)
                 if (feat[i]=='Prime mover (no of trailers unknown) '){
@@ -107,53 +113,56 @@ function BarChart(input) {
 
                 }
             }
-            else if (option == 'Region' || option == 'Atmospheric Condition' || option=='Surface Condition' || option=='Light Condition' || option == 'LGA'){
+            else if (option == 'Region' || option == 'Atmospheric Condition (Top 10)' || option=='Surface Condition (Top 10)' || option=='Light Condition' || option == 'LGA (Top 10)'){
                 feat[i]=`${feat[i]} `
                 feat[i]=feat[i][0].toUpperCase() + feat[i].slice(1)
         }
         }
         // For certain options, only showing top 10
-        if (option=='LGA' || option=='Vehicle Type' || option=='Vehicle Brand' || option=='Atmospheric Condition'|| option=='Speed Zone'|| option=='Surface Condition'){
-            top_10_accident=accident_count_copy.sort(function(a, b){return b - a}).splice(0,10);
+        if (option=='LGA (Top 10)' || option=='Vehicle Type (Top 10)' || option=='Vehicle Brand (Top 10)' || option=='Atmospheric Condition (Top 10)'|| option=='Speed Zone (Top 10)'|| option=='Surface Condition (Top 10)'){
+            top_10_accident=accident_count_copy.sort(function(a, b){return b - a}).splice(0,10); // sorting top ten 
             top_10_features=[];
             let index = 0;
             for (let i=0;i<10;i++){
-                index=accident_count.indexOf(top_10_accident[i]);
-                top_10_features.push(feat[index]);
+                index=accident_count.indexOf(top_10_accident[i]); // finding indices of top ten accidents 
+                top_10_features.push(feat[index]); // finding features corresponding to top ten accidents
             }
             accident_count=top_10_accident.reverse();
             feat=top_10_features.reverse();
         }
-        let bar_data = [{
-            x: accident_count,
-            y: feat,
-            type: "bar",
-            orientation: 'h',
-            marker: {
-                color: 'green'
-            }
-        }];
-
-        // Define the layout with opacity
-        let layout = {
-            yaxis: {
-                automargin: true,
-            },
-            xaxis: {
-                title: {
-                    text: "Number of Accidents",
+        let x_label_text = 'Number of Accidents' // Choosing x label 
+        if (option == 'Age Group'){
+            x_label_text='Number of People'
+        }
+        else if (option == 'Vehicle Type (Top 10)'||option == 'Vehicle Brand (Top 10)'){
+            x_label_text='Number of Vehicles'
+        }
+            let bar_data = [{  
+                x: accident_count,
+                y: feat,
+                type: "bar",
+                orientation:'h',
+                marker: {
+                    color: 'green'
                 }
-            },
-            bargap: 0.3, // Adjust the gap between bars
-            // Set the background color of the plot area to make it opaque
-            paper_bgcolor: 'rgba(255, 255, 255, 0.1)', // Opaque white color for the entire chart background
-            plot_bgcolor: 'rgba(255, 255, 255, 0.1)',
-        };
-
-        // Create the bar chart with the specified layout
-        Plotly.newPlot("bar", bar_data, layout);
-    });
-}
+            }];
+            let layout = {
+                yaxis: {
+                    automargin: true,
+                  },
+                xaxis: {
+                    title: {
+                        text: x_label_text,
+                      }
+                  },
+                  bargap: 0.3, // Adjust the gap between bars
+                  // Set the background color of the plot area to make it opaque
+                  paper_bgcolor: 'rgba(255, 255, 255, 0.1)', // Opaque white color for the entire chart background
+                  plot_bgcolor: 'rgba(255, 255, 255, 0.1)',
+              }
+            Plotly.newPlot("bar", bar_data,layout); // Plotting data 
+        }
+    )};
 
 
 // // Assigning variable to div with class = selDatasetFeat
@@ -176,7 +185,7 @@ function init(){
     BarChart(['2020','Locality Type']);
     // Put options in the dropdown menu 
     years = [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
-    features=['Locality Type','LGA','Region','Atmospheric Condition','Surface Condition','Light Condition','Speed Zone','Age Group','Vehicle Type','Vehicle Brand']
+    features=['Locality Type','LGA (Top 10)','Region','Atmospheric Condition (Top 10)','Surface Condition (Top 10)','Light Condition','Speed Zone (Top 10)','Age Group','Vehicle Type (Top 10)','Vehicle Brand (Top 10)']
     for (let i = 0;i<features.length;i++){
         dropdownMenuFeat.append("option").text(features[i]);
     }
